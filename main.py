9a5c813eb91d43be38553ca5a11feb410f3719dd
philippe_pras <ph.pras@gmail.com>
2023-11-14 19:15:16 +0100

    Udate connect DB

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from db import engine, session
 from models import meta_base
 
-
+# Epic_event_postgresql
+# Pw 12345678
+# Port number 5432
+# Pw Table Epic Event > 12345678
+# bd : epic_event_db
+# user : epic_event_user
 
 
 
